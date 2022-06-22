7a591dd0121137ddb84d97a6aa499f5d53956eec
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2022-06-23 00:23:28 +0300

    lab-2 done

--- main.c
+++ main.c
@@ -3,5 +3,19 @@
 #include <locale.h>
 
 int main() {
+    float x;
+    double topStatement,
+            bottomStatement,
+            result,
+            sinus;
 
+    printf("Введите переменную: \n");
+    scanf("%f", &x);
+
+    sinus = sin(pow((double) (2 * x) + 19.0 / 180 * M_PI, 3));
+    topStatement = sinus * pow(M_E, (double) (-2 * x));
+    bottomStatement = pow(sqrt(fabs(4 - sqrt(fabs((double) (x - 2) * sin((double) x))))), 3);
+    result = topStatement / bottomStatement + 1.0 / 3 * tan((double) x) * log(fabs(pow((double) x, 3))) / M_LN2;
+
+    printf("Результат: %f \n", result);
 }
