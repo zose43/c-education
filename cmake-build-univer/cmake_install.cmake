26f2b20cdeb2684e76ee427d1c5aad5e39101ba0
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2022-06-25 21:30:29 +0300

    lab-5 done

--- cmake-build-univer/cmake_install.cmake
+++ cmake-build-univer/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/zose/CLionProjects/univer/c-education
+# Install script for directory: /home/zose/c_projects/univer/c-education
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/zose/CLionProjects/univer/c-education/cmake-build-univer/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/zose/c_projects/univer/c-education/cmake-build-univer/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
--- main.c
+++ main.c
@@ -2,6 +2,30 @@
 #include <math.h>
 #include <locale.h>
 
-int main() {
+int getFactorial(int num);
 
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    int n = 1;
+    double number, sum = 0;
+
+    printf("Введите число:\n");
+    scanf("%lf", &number);
+
+    while (sum == number) {
+        sum += exp(2 * n + 1) + (double) n / sqrt(getFactorial(n));
+        n++;
+    }
+
+    return 0;
+}
+
+int getFactorial(int num) {
+    int i, result = 1;
+    for (i = 1; i <= num; i++) {
+        result *= i;
+    }
+
+    return result;
 }
