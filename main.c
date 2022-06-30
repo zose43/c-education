b253fab8ebfbaed717a15c4271c16d34cca1c4f6
Kirill Zorin <thesecretsoftheuniverse@mail.ru>
2022-06-30 13:16:10 +0300

    fx lab-1

--- main.c
+++ main.c
@@ -4,27 +4,14 @@
 
 int main() {
     setlocale(LC_ALL, "RUS");
-    double hypotenuse,
-            S,
-            S_triangle, S_sector;
-    int radius,
-            P,
-            P_triangle, P_sector;
+    double S, P, radius;
 
     printf("Введите радиус круга: \n");
-    scanf("%d", &radius);
+    scanf("%lf", &radius);
 
-    hypotenuse = pow(radius, 2) + pow(radius, 2);
+    S = M_PI * pow(radius, 2) / 4;
+    P = pow(sqrt(pow(radius, 2) * pow(radius, 2)) + radius, 2) + 0.5 * M_PI * radius;
+    printf("Общая S фигуры: %lf, Общий P фигуры: %lf\n", S, P);
 
-    P_triangle = (int) sqrt(hypotenuse) + radius + (int) hypotenuse;
-    P_sector = M_PI * radius * 0.5;
-
-    S_sector = M_PI * pow(radius, 2) * 0.25;
-    S_triangle = 0.5 * pow(radius, 2);
-
-    P = (radius * 4 - P_sector) + P_triangle;
-    S = (pow(radius, 2) - S_sector) + S_triangle;
-
-    printf("Общая S фигуры: %lf, Общий P фигуры: %d\n", S, P);
     return 0;
 }
